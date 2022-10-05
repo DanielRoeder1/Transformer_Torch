f4e5c87e5f97e1cd17722ccdab9d9a5bb3ec409a
DanielRoeder1 <roeder_daniel_97@web.de>
2022-10-05 21:55:57 +0200

    Fix forward of transformer

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+from base64 import encode
 import torch
 from numpy import sqrt
 from torch import nn
@@ -22,6 +23,7 @@ class Transformer(nn.Module):
     
         self.pad_id = config.pad_idx
         self.register_buffer("attention_mask", torch.tril(torch.ones(config.seq_len, config.seq_len)).view(1,1,config.seq_len, config.seq_len).bool())
+        #self.register_buffer("init_seq", torch.LongTensor([[config.bos_idx]]))
 
     def forward(self,src_ids, trgt_ids):
         src_mask = self.get_pad_mask(src_ids)
@@ -30,7 +32,7 @@ class Transformer(nn.Module):
         src_embed = self.embedding_layer(src_ids)
         trgt_embed = self.embedding_layer(trgt_ids)
 
-        enc_out = self.encoder_stack((src_embed,src_mask))
+        enc_out, _ = self.encoder_stack((src_embed,src_mask))
 
         for dec in self.decoder_stack:
             trgt_embed = dec(trgt_embed,trgt_mask, enc_out, src_mask)
@@ -39,6 +41,15 @@ class Transformer(nn.Module):
         #trgt_embed = self.softmax(trgt_embed)
         return trgt_embed 
     
+    # def translate(self, src_ids):
+    #     src_mask = self.get_pad_mask(src_ids)
+    #     src_embed = self.embedding_layer(src_ids)
+    #     enc_out = self.encoder_stack((src_embed,src_mask))
+    
+    #     for dec in self.decoder_stack:
+    #         trgt_embed = dec(trgt_embed,trgt_mask, enc_out, src_mask)
+
+
     def get_pad_mask(self,input_ids):
         # This expands the dim to [batch, 1,1,seq_len]
         return (input_ids != self.pad_id)[:,None,None,:]
@@ -75,7 +86,7 @@ class DecoderBlock(nn.Module):
     def __init__(self, config) -> None:
         super().__init__()
         
-        self.masked_attention = MultiHeadAttentionFAST(config, masked_attention=True)
+        self.masked_attention = MultiHeadAttentionFAST(config)
         self.enc_dec_attention = MultiHeadAttentionFAST(config)
         self.feed_forward = FeedForward(config)
 
@@ -118,19 +129,19 @@ class EmbeddingBlock(nn.Module):
 if __name__ == "__main__":
     tokenizer = get_Tokenizer()
     config = ConfigObject("config.json")
-    config.update({"vocab_size": len(tokenizer), "pad_idx": tokenizer.pad_token_id})
+    config.update({"vocab_size": len(tokenizer), "pad_idx": tokenizer.pad_token_id, "bos_idx": tokenizer.bos_token_id})
     transformer = Transformer(config)
+    transformer.load_state_dict(torch.load("results/0.07.pth"))
 
-    out = tokenizer(["Wir waren im Park am Sonntag", "Ich war gestern in der Bahn"], text_target =["We were in the park on Sunday", "I was in the train yesterday"], add_special_tokens= True, max_length= config.seq_len+1, padding= "max_length", return_token_type_ids= False, return_attention_mask= False, return_tensors="pt")
-    src = out["input_ids"]
+    out = tokenizer(["Wir waren im Park am Sonntag"],text_target= ["We were in the park on sunday"], add_special_tokens= True, max_length= config.seq_len+1, padding= "max_length", return_token_type_ids= False, return_attention_mask= False, return_tensors="pt")
+    src = out["input_ids"][:,:-1]
     trgt = out["labels"]
-    src = src[:,:-1]
     trgt_in = trgt[:,:-1]
     trgt_label = trgt[:,1:]
 
-    transformer.eval()
     output = transformer(src, trgt_in)
-    out_txt = tokenizer.batch_decode(output.argmax(-1))
-    out_label = tokenizer.batch_decode(trgt_label)
+    pred = output.argmax(2)
+    print(pred)
+    print(tokenizer.decode(pred[0]))
 
     print(f"Trainable parameters Transformer: {get_praram_count(transformer)}")
